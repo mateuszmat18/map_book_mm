2a3b64f19288a2969afa7462496a52cf2a598b9e
mateuszmat18 <166720077+mateuszmat18@users.noreply.github.com>
2024-04-18 12:50:36 +0200

    feat:added read functionality

---
+++ models/data.py
@@ -0,0 +1,7 @@
+users: list[dict] = [
+    {"name": "Mateusz", "surname": "Matysiak", "posts": 33},
+    {"name": "Janek", "surname": "Mielec", "posts": 20},
+    {"name": "Maciej", "surname": "Przybytek", "posts": 45},
+    {"name": "Bartosz", "surname": "Pietrasik", "posts": 60},
+    {"name": "Tymoteusz", "surname": "Miszczak", "posts": 21},
+]
