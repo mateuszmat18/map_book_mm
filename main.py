eb394613047b114cff2ac59dab01b241c4167d37
mateuszmat18 <166720077+mateuszmat18@users.noreply.github.com>
2024-04-18 14:25:14 +0200

    feat:added search user

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.crud import read
+from utils.crud import read, create_user, search
 from models.data import users
 
 
@@ -9,11 +9,17 @@ if __name__ == '__main__':
         print("Menu:")
         print("0. Zakończ program:")
         print("1.Pokaż co u znajomych: ")
+        print("2.Dodaj znajomego: ")
+        print("3.Pokaż co u znajomych: ")
         menu_option: str = input("Wybierz dostępną funkcję z menu: ")
         if menu_option == "0":
             break
         if menu_option == "1":
             read(users)
+        if menu_option == "2":
+            create_user(users)
+        if menu_option == "3":
+            search(users)
 
 
 
--- utils/crud.py
+++ utils/crud.py
@@ -1,4 +1,18 @@
-
-def read(users: list[dict])->None:
+def read(users: list[dict]) -> None:
     for user in users[1:]:
         print(f'Twój znajomy {user["name"]} opublikował: {user["posts"]}')
+
+
+def create_user(users: list[dict]) -> None:
+    name: str = input("Podaj imię użytkownika: ")
+    surname: str = input("Podaj nazwisko użytkownika: ")
+    posts: int = int(input("Podaj liczbę postów użytkownika: "))
+    user: dict = {"name": name, "surname": surname, "posts": posts}
+    users.append(user)
+
+
+def search(users: list[dict]) -> None:
+    user_name: str = input("Kogo szukasz: ")
+    for user in users[1:]:
+        if user["name"] == user_name:
+            print(f'Twój znajomy {user["name"]} opublikował: {user["posts"]}')
